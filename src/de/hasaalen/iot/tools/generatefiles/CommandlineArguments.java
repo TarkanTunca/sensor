602edaa6a6b10665fed29ae2667a4cdc3cb49865
Marcus Gelderie <redmnic@gmail.com>
2019-04-11 10:29:07 +0200

    Make output directory configurable
    Add a command-line argument to make
    the output directory configurable.
    (Exercise 6).

--- src/de/hasaalen/iot/tools/generatefiles/CommandlineArguments.java
+++ src/de/hasaalen/iot/tools/generatefiles/CommandlineArguments.java
@@ -4,33 +4,36 @@ class CommandlineArguments {
     final int numFiles;
     final int numLines;
     final Sensor type;
+    final String directory;
 
     CommandlineArguments() {
         numFiles = 1;
         numLines = 100;
+        directory = ""; // this will cause the current directory to be used.
 
         // null here will cause a random sensor type to be selected
         type = null;
     }
 
-    CommandlineArguments(int numFiles, int numLines, Sensor type) {
+    CommandlineArguments(String directory, int numFiles, int numLines, Sensor type) {
+        this.directory = directory;
         this.type = type;
         this.numLines = numLines;
         this.numFiles = numFiles;
     }
 
     public static CommandlineArguments fromStringArray(String[] args) {
-        //TODO: Adapt this method to accept an extra parameter 'directory' as first argument
-        if (args == null || args.length < 2) {
+        if (args == null || args.length < 3) {
             return null;
         }
+        final String directory = args[0];
         //TODO: Exceptions
-        final int numFiles = Integer.parseInt(args[0]);
-        final int numLines = Integer.parseInt(args[1]);
+        final int numFiles = Integer.parseInt(args[1]);
+        final int numLines = Integer.parseInt(args[2]);
 
         Sensor type = null;
-        if (args.length >= 3) {
-            switch (args[2]) {
+        if (args.length >= 4) {
+            switch (args[3]) {
                 case "humidity":
                     type = Sensor.HUMIDITY;
                     break;
@@ -46,6 +49,6 @@ class CommandlineArguments {
                     break;
             }
         }
-        return new CommandlineArguments(numFiles, numLines, type);
+        return new CommandlineArguments(directory, numFiles, numLines, type);
     }
 }
--- tests/de/hasaalen/iot/tools/generatefiles/CommandlineArgumentsTest.java
+++ tests/de/hasaalen/iot/tools/generatefiles/CommandlineArgumentsTest.java
@@ -14,19 +14,22 @@ class CommandlineArgumentsTest {
         assertThat(a.type, is(nullValue()));
         assertThat(a.numFiles, is(equalTo(1)));
         assertThat(a.numLines, is(equalTo(100)));
+        assertThat(a.directory, is(emptyString()));
     }
 
     @Test
     void testConstructor() {
-        CommandlineArguments a = new CommandlineArguments(2, 20, Sensor.HUMIDITY);
+        CommandlineArguments a = new CommandlineArguments("foo", 2, 20, Sensor.HUMIDITY);
+        assertThat(a.directory, is(equalTo("foo")));
         assertThat(a.type, is(equalTo(Sensor.HUMIDITY)));
         assertThat(a.numFiles, is(equalTo(2)));
         assertThat(a.numLines, is(equalTo(20)));
     }
 
     @Test
-    void fromStringArray() {
-        String[] oneArg = {"23"};
+    void testTooFewArgs() {
+        String[] oneArg = {"dir"};
+        String[] twoArg =  {"dir", "23"};
         /* If a null array is passed, or if the array contains less than two entries,
         a null value should be returned to indicate an error.
          */
@@ -34,23 +37,27 @@ class CommandlineArgumentsTest {
         assertThat(defaultArgs, is(nullValue()));
         CommandlineArguments fromOneArg = CommandlineArguments.fromStringArray(oneArg);
         assertThat(fromOneArg, is(nullValue()));
+        CommandlineArguments fromTwoArg = CommandlineArguments.fromStringArray(twoArg);
+        assertThat(fromTwoArg, is(nullValue()));
+    }
+
+    @Test
+    void testFourArgs() {
+        String[] threeArgs = {"dir", "33", "714", "humidity"};
+        CommandlineArguments fromFourArgs = CommandlineArguments.fromStringArray(threeArgs);
+        assertThat(fromFourArgs.directory, is(equalTo("dir")));
+        assertThat(fromFourArgs.numFiles, equalTo(33));
+        assertThat(fromFourArgs.numLines, equalTo(714));
+        assertThat(fromFourArgs.type, is(equalTo(Sensor.HUMIDITY)));
     }
 
     @Test
     void testThreeArgs() {
-        String[] threeArgs = {"33", "714", "humidity"};
-        CommandlineArguments fromThreeArgs = CommandlineArguments.fromStringArray(threeArgs);
-        assertThat(fromThreeArgs.numFiles, equalTo(33));
-        assertThat(fromThreeArgs.numLines, equalTo(714));
-        assertThat(fromThreeArgs.type, is(equalTo(Sensor.HUMIDITY)));
-    }
-
-    @Test
-    void testTwoArgs() {
-        String[] twoArgs = {"34", "134"};
-        CommandlineArguments fromTwoArgs = CommandlineArguments.fromStringArray(twoArgs);
-        assertThat(fromTwoArgs.numFiles, equalTo(34));
-        assertThat(fromTwoArgs.numLines, equalTo(134));
-        assertThat(fromTwoArgs.type, is(nullValue()));
+        String[] twoArgs = {"dir", "34", "134"};
+        CommandlineArguments fromThreeArgs = CommandlineArguments.fromStringArray(twoArgs);
+        assertThat(fromThreeArgs.directory, is(equalTo("dir")));
+        assertThat(fromThreeArgs.numFiles, equalTo(34));
+        assertThat(fromThreeArgs.numLines, equalTo(134));
+        assertThat(fromThreeArgs.type, is(nullValue()));
     }
 }
